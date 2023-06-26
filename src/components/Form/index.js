fd2a3f93e63ffd7db4566ea6cc9f027039d300c4
Niko Kreissl <nikokreissl@gmail.com>
2023-06-26 18:04:32 +0200

    Remove primereact

---
+++ src/components/Form/index.js
@@ -0,0 +1,7 @@
+export default function Form() {
+  return (
+    <form>
+      <input type="text" name="first-name" id="last-name" />
+    </form>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import HeaderBar from "./components/Header-Bar";
+import Form from "./components/Form";
+import "primereact/resources/themes/lara-light-indigo/theme.css";
+import "primereact/resources/primereact.min.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <HeaderBar />
-    <h1>Hello</h1>
+    <Form />
   </React.StrictMode>
 );
